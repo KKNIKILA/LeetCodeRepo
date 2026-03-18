9a7229bc5870947d2841a38c64596e2fd76f0129
KKNIKILA <nikilak203@gmail.com>
2026-03-18 22:07:37 +0530

    Time: 7 ms (11.3%), Space: 47.7 MB (43.36%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -7,17 +7,17 @@ class Solution
             String string=words[i];
             int p1=0;
             int p2=string.length()-1;
-            int flag=1;
+            int NumbeR=1;
             while(p1<p2)
             {
                 if(string.charAt(p1)!=string.charAt(p2))
                 {
-                    flag=0;
+                    NumbeR=0;
                 }
                 p1++;
                 p2--;
             }
-            if(flag==1)
+            if(NumbeR==1)
             {
                 return words[i];
             }
