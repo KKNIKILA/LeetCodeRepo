e991ce7fc355b9a2e2211b60035ac5a8073a81e7
KKNIKILA <nikilak203@gmail.com>
2026-03-25 20:34:41 +0530

    Time: 3 ms (81.33%), Space: 43.8 MB (22.62%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,38 @@
+class Solution 
+{
+    public int calPoints(String[] operations) 
+    {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<operations.length;i++)
+        {
+            if(operations[i].equals("D"))
+            {
+                st.push(2*(st.peek()));
+            }
+            else if(operations[i].equals("C"))
+            {
+                st.pop();
+            }
+            else if(operations[i].equals("+"))
+            {
+                int a=st.pop();
+                int b=st.pop();
+                int c=b+a;
+                st.push(b);
+                st.push(a);
+                st.push(c);
+            }
+            else
+            {
+                st.push(Integer.parseInt(operations[i]));
+            }
+        }
+        int NumbeR=0;
+        while(!(st.isEmpty()))
+        {
+            NumbeR=NumbeR+st.peek();
+            st.pop();
+        }
+        return NumbeR;
+    }
+}
