59df9f27b6a935d4394afb120ca8699c0540fc84
KKNIKILA <nikilak203@gmail.com>
2026-04-10 21:59:06 +0530

    Time: 1 ms (98.66%), Space: 47.5 MB (29.09%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public int pivotIndex(int[] nums) 
+    {
+        int l=0;
+        int r=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            r=r+nums[i];
+        }
+        for(int i=0;i<nums.length;i++)
+        {
+            r=r-nums[i];
+            if(l==r)
+            {
+                return i;
+            }
+            l=l+nums[i];
+        }
+        return -1;
+    }
+}
