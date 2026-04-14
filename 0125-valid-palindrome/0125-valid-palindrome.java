ae9623cee16838ca883637ebe1377cdbf3a1c3c2
KKNIKILA <nikilak203@gmail.com>
2026-04-14 12:09:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,29 +2,6 @@ class Solution
 {
     public boolean isPalindrome(String s) 
     {
-        // String NumbeR="";
-        // s=s.toLowerCase();
-        // for(int i=0;i<s.length();i++)
-        // {
-        //     char c=s.charAt(i);
-        //     if(((c>='a') && (c<='z'))||(((c>='0') && (c<='9'))))
-        //     {
-        //         NumbeR=NumbeR+c;
-        //     }
-        // }
-        // s=NumbeR;
-        // int i=0;
-        // int j=s.length()-1;
-        // while(i<j)
-        // {
-        //     if(s.charAt(i)!=s.charAt(j))
-        //     {
-        //         return false;
-        //     }
-        //     i++;
-        //     j--;
-        // }
-        // return true;
         int NumbeR;
         int i=0;
         int j=s.length()-1;
