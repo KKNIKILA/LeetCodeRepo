a21ef0905119e3317cb3a7de29cd3292805e9abb
KKNIKILA <nikilak203@gmail.com>
2026-04-17 22:56:25 +0530

    Time: 1 ms (22.26%), Space: 48.1 MB (78.41%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,18 @@
+class Solution 
+{
+    public void reverseString(char[] s) 
+    {
+        int NumbeR;
+        int i=0;
+        int j=s.length-1;
+        while(i<j)
+        {
+            char ch=s[i];
+            s[i]=s[j];
+            s[j]=ch;
+            i++;
+            j--;
+        }
+        return;
+    }
+}
