499c72a73be6119f0c09bf5ce29b434f295ad553
KKNIKILA <nikilak203@gmail.com>
2026-04-20 15:32:36 +0530

    Time: 5 ms (13.27%), Space: 44.9 MB (10.73%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public String addBinary(String a, String b) 
+    {
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        String string="";
+        while((i>=0) || (j>=0) || (carry==1))
+        {
+            int f=0;
+            int s=0;
+            if(i>=0)
+            {
+                f=a.charAt(i)-'0';
+            }
+            if(j>=0)
+            {
+                s=b.charAt(j)-'0';
+            }
+            int tot=f+s+carry;
+            string=(tot%2)+string;
+            carry=tot/2;
+            i--;
+            j--;
+        }
+        return string;
+    }
+}
