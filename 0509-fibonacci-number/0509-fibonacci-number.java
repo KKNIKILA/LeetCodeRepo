51de062108af892380150affe5450ccedc2a52b2
KKNIKILA <nikilak203@gmail.com>
2026-05-21 19:49:28 +0530

    Time: 12 ms (9.12%), Space: 42.1 MB (32.95%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -3,11 +3,7 @@ class Solution
     public int fib(int n) 
     {
         int NumbeR;
-        if(n<0)
-        {
-            return 0;
-        }
-        if((n==0) || (n==1))
+        if((n<=0) || (n==1))
         {
             return n;
         }
