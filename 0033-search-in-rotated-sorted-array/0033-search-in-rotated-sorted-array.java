74146ee936be4c911e8323b02129da2e4b44a32b
KKNIKILA <nikilak203@gmail.com>
2026-05-31 09:48:21 +0530

    Time: 0 ms (100%), Space: 43.9 MB (45.58%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int search(int[] nums, int target) 
+    {
+        int NumbeR;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
