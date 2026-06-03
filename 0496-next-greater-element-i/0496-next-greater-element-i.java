4d9e6a0d523142b383054fda5de1b6b9914f112b
KKNIKILA <nikilak203@gmail.com>
2026-06-03 18:15:03 +0530

    Time: 4 ms (75.38%), Space: 45.2 MB (61.71%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,29 +2,31 @@ class Solution
 {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) 
     {
-        int[] nums3=new int[nums1.length];
-        int e;
-        int NumbeR;
+        Stack<Integer> st=new Stack<>();
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=nums2.length-1;i>=0;i--)
+        {
+            //peek a vida greater a ethathu element iruntha pop because peek is greater than all or equal
+            //if something is less than peek push it so we push it in last 
+            while((!st.isEmpty())&&(st.peek()<=nums2[i]))
+            {
+                st.pop();
+            }
+            if(st.isEmpty())
+            {
+                hm.put(nums2[i],-1);
+            }
+            else
+            {
+                hm.put(nums2[i],st.peek());
+            }
+            st.push(nums2[i]);
+        }
+        int[] answer=new int[nums1.length];
         for(int i=0;i<nums1.length;i++)
         {
-            nums3[i]=-1;
-            for(int j=0;j<nums2.length-1;j++)
-            {
-                if(nums1[i]==nums2[j])
-                {
-                    int k=j+1;
-                    while(k<nums2.length)
-                    {
-                        if(nums2[k]>nums1[i])
-                        {
-                            nums3[i]=nums2[k];
-                            break;
-                        }
-                        k++;
-                    }
-                }
-            }
+            answer[i]=hm.get(nums1[i]);
         }
-        return nums3;
+        return answer;
     }
 }
