b3bd46b28b8968691e471cbfe440723bd49cec3c
KKNIKILA <nikilak203@gmail.com>
2026-06-05 22:11:06 +0530

    Time: 349 ms (35.4%), Space: 49.5 MB (84.34%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution 
+{
+    public int[] nextLargerNodes(ListNode head) 
+    {
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next=null;
+        ListNode temp=head;
+        int count=0;
+        while(temp!=null)
+        {
+            count++;
+            temp=temp.next;
+        }
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        temp=prev;
+        while(temp!=null)
+        {
+            System.out.println(temp.val+" ");
+            temp=temp.next;
+        }
+        int index=count-1;
+        int[] newarr=new int[count]; 
+        Stack<Integer> s=new Stack<>();
+        temp=prev;
+        while(temp!=null)
+        {
+            while((!s.isEmpty()) && (s.peek()<=temp.val))
+            {
+                s.pop();
+            }
+            if((!s.isEmpty()) && (s.peek()>temp.val))
+            {
+                newarr[index--]=s.peek();
+            }
+            else
+            {
+                newarr[index--]=0;
+            }
+            s.push(temp.val);
+            temp=temp.next;
+        }
+        return newarr;   
+    }
+}
