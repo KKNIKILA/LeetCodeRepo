3dbd6ef81e57972acf93b197080107e08a579554
KKNIKILA <nikilak203@gmail.com>
2026-06-11 11:10:06 +0530

    Time: 6 ms (5.77%), Space: 43.9 MB (5.83%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,32 +2,24 @@ class Solution
 {
     public boolean isValid(String s) 
     {
-        int NumbeR;
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++)
         {
-            if((s.charAt(i)=='(')||(s.charAt(i)=='{')||(s.charAt(i)=='['))
+            if((s.charAt(i)==')') || (s.charAt(i)=='}') || (s.charAt(i)==']'))
             {
-                st.push(s.charAt(i));
-            }
-            else if(st.isEmpty())
-            {
-                return false;
-            }
-            else if(s.charAt(i)==']')
-            {
-                if(st.peek()=='[')
-                {
-                    st.pop();
-                }
-                else
+                if(st.isEmpty())
                 {
                     return false;
                 }
-            }
-            else if(s.charAt(i)=='}')
-            {
-                if(st.peek()=='{')
+                else if((s.charAt(i)==')')&&(st.peek()=='('))
+                {
+                    st.pop();
+                }
+                else if((s.charAt(i)=='}')&&(st.peek()=='{'))
+                {
+                    st.pop();
+                }
+                else if((s.charAt(i)==']')&&(st.peek()=='['))
                 {
                     st.pop();
                 }
@@ -38,14 +30,7 @@ class Solution
             }
             else
             {
-                if(st.peek()=='(')
-                {
-                    st.pop();
-                }
-                else
-                {
-                    return false;
-                }
+                st.push(s.charAt(i));
             }
         }
         if(st.isEmpty())
