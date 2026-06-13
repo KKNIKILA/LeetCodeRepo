027b0a0d111c4b8cb9978d9861eb6407b9168342
KKNIKILA <nikilak203@gmail.com>
2026-06-13 11:40:26 +0530

    Time: 512 ms (10.05%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,2 +1,28 @@
 # Write your MySQL query statement below
 select author_id as id from views where author_id=viewer_id group by author_id order by author_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#NumbeR
