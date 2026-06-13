5e895781fb9d89f6d80389b7e2eb65077d77f9be
KKNIKILA <nikilak203@gmail.com>
2026-06-13 17:19:40 +0530

    Time: 432 ms (67.95%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,2 +1,31 @@
 # Write your MySQL query statement below
 select max(num) as num from (select num from MyNumbers group by num having count(num)=1) t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#NumbeR
