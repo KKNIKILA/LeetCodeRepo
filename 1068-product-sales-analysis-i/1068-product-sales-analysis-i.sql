11b292eaecf1df690f2210fcd53f66649a4a93b9
KKNIKILA <nikilak203@gmail.com>
2026-06-13 11:56:04 +0530

    Time: 1770 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,2 +1,25 @@
 # Write your MySQL query statement below
 select p2.product_name,p1.year,p1.price from Sales p1 left join product p2 on p1.product_id=p2.product_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#NumbeR
