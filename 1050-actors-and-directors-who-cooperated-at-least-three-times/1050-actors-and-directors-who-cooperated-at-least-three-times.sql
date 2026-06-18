0800d5c1d5e552ef99dfbe9373a6de3878164a6d
KKNIKILA <nikilak203@gmail.com>
2026-06-18 17:34:07 +0530

    Time: 416 ms (17.97%), Space: 0B (100%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,2 +1,20 @@
 # Write your MySQL query statement below
 select actor_id,director_id from ActorDirector group by actor_id,director_id having count(actor_id)>=3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#NumbeR
