4a9d8bdd6a36240eb143f35b6e0f60f79bfaf5d1
KKNIKILA <nikilak203@gmail.com>
2026-09-08 13:58:48 +0530

    Time: 1 ms (83.65%), Space: 42.8 MB (8.32%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -6,14 +6,9 @@ class Solution
         int digitCount=0;
         int copy=n;
         int NumbeR;
-        while(n>0)
+        if((n/1000)>0)
         {
-            digitCount++;
-            n=n/10;
-        }
-        if(digitCount>3)
-        {
-            count=count+(copy-999);
+            count=count+(n-999);
         }
         return count;
     }
