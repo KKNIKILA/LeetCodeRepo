201de1f76abe15ce04b0fcf66bbae7c611ea5ed1
KKNIKILA <nikilak203@gmail.com>
2026-09-08 13:57:17 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (85.26%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,19 @@
+class Solution 
+{
+    public int countCommas(int n) 
+    {
+        int count=0;
+        int digitCount=0;
+        int copy=n;
+        while(n>0)
+        {
+            digitCount++;
+            n=n/10;
+        }
+        if(digitCount>3)
+        {
+            count=count+(copy-999);
+        }
+        return count;
+    }
+}
