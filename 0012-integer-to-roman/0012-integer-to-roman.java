4ddb9eafc4cdeb48cf50f02bb63b0ca52b9a14da
KKNIKILA <nikilak203@gmail.com>
2026-09-10 09:55:54 +0530

    Time: 5 ms (19.72%), Space: 46.6 MB (25.2%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -3,6 +3,7 @@ class Solution
     public String intToRoman(int num) 
     {
         int[] newarr1={1,4,5,9,10,40,50,90,100,400,500,900,1000};
+        int NumbeR;
         String[] newarr2={"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
         String string="";
         for(int i=12;i>=0;i--)
